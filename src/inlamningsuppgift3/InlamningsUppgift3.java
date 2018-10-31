9193cc4db2930045901d4a8ca3588dfed3fda6e0
Marcstro <marcus.strom.90@gmail.com>
2018-10-31 13:21:49 +0100

    Behovs enbart snygga till programmet

--- src/inlamningsuppgift3/InlamningsUppgift3.java
+++ src/inlamningsuppgift3/InlamningsUppgift3.java
@@ -33,20 +33,13 @@ public class InlamningsUppgift3 extends JFrame implements ActionListener{
     int tompos;
     JLabel GameBoard = new JLabel();
     
-    
     String teststring01;
-    //public  InlamningsUppgift3(){
-        
-        
-         
-        // GameBoard = new JLabel();
+    
         JLabel ButtonRow = new JLabel();
-        JButton ordning = new JButton("Ordningen");
-        JButton info = new JButton("INFO");
         JButton scramble = new JButton("Scramble");
+        JButton fuskScramble = new JButton("FuskScramble");
         JLabel winst = new JLabel();
         
-        //GameBoard.setSize(350,350);
         JButton l1 = new JButton("1");
         JButton l2 = new JButton("2");
         JButton l3 = new JButton("3");
@@ -67,25 +60,13 @@ public class InlamningsUppgift3 extends JFrame implements ActionListener{
         
         
         public  InlamningsUppgift3(){
-            tomma.setEnabled( false );
-           // l12.setActionCommand("testar03");
+            
+        tomma.setEnabled( false );
         GameBoard.setPreferredSize(new Dimension(355, 355));
-        
         GameBoard.setLayout(new GridLayout(4,4, 5, 5));
         
-        
-        
-        
         lablarna = new LinkedList<>();
-        
-        
-       // l1.setPreferredSize(new Dimension(350, 140));
-        //Lyssnaren ml = new Lyssnaren();
-        //l1.addMouseListener(ml);
-        
-        //this.addFocusListener(fl);
-        lablarna.add(l1);lablarna.add(l2);lablarna.add(l3);
-        lablarna.add(l4);
+        lablarna.add(l1);lablarna.add(l2);lablarna.add(l3);lablarna.add(l4);
         lablarna.add(l5);lablarna.add(l6);lablarna.add(l7);
         lablarna.add(l8);lablarna.add(l9);lablarna.add(l10);
         lablarna.add(l11);lablarna.add(l12);lablarna.add(l13);
@@ -100,80 +81,41 @@ public class InlamningsUppgift3 extends JFrame implements ActionListener{
         for(JButton l: lablarna){
             l.setFont(new Font("Arial", Font.PLAIN, 40));
             GameBoard.add(l);
-            //l.addMouseListener(this);
             l.addActionListener(this);
             l.setVisible(true);
             l.setSize(50,50);
             l.setPreferredSize(new Dimension(55, 55));
             l.setName(Integer.toString(raknare));
-            //l.setName("Hejsan");
             raknare++;
         }
         GameBoard.add(tomma);
         add(GameBoard);
-        //GameBoard.setVisible(true);
-        
-        //SortaAllt(lablarna);
-        
         
         setLayout( new FlowLayout() );
         
-        
         ButtonRow.setLayout( new FlowLayout() );
         
-        
-        ButtonRow.add(ordning);
         ButtonRow.add(scramble);
-        ButtonRow.add(info);
-        ButtonRow.add(winst);
+        ButtonRow.add(fuskScramble);
         ButtonRow.setVisible(true);
         ButtonRow.setPreferredSize(new Dimension(355, 355));
-        add(ButtonRow);
         
+        add(ButtonRow);
         
         scramble.addActionListener(this);
         scramble.setVisible(true);
         scramble.setSize(150,150);
         scramble.setPreferredSize(new Dimension(155, 155));
         
-        //scramble.addActionListener(this);
-        boolean vunnit = false;
-        winst.setText(String.valueOf(vunnit));
-        winst.setVisible(true);
-        winst.setSize(150,150);
-        winst.setPreferredSize(new Dimension(155, 155));
+        fuskScramble.addActionListener(this);
+        fuskScramble.setVisible(true);
+        fuskScramble.setSize(150,150);
+        fuskScramble.setPreferredSize(new Dimension(155, 155));
         
-        info.addActionListener(this);
-        info.setVisible(true);
-        info.setSize(150,150);
-        info.setPreferredSize(new Dimension(155, 155));
-        
-        ordning.addActionListener(this);
-        ordning.setVisible(true);
-        ordning.setSize(150,150);
-        ordning.setPreferredSize(new Dimension(155, 155));
-        
-        
-//        //l9.setSize(150,150);
-//        l9.setPreferredSize(new Dimension(350, 140));
-//       l9.add(l10);
-//       l10.setPreferredSize(new Dimension(55, 55));
-//        l9.add(l11);
-//        add(l9);
-//        l10.setVisible(true);
-//        l11.setVisible(true);
-//        l9.setLayout( new FlowLayout() );
-        
-         
-//        invalidate();
-//        repaint();
-   
         setSize(1200,1100);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         pack();
-        
-        
         
     }
     public void BytPos(int knappP){
@@ -187,6 +129,10 @@ public class InlamningsUppgift3 extends JFrame implements ActionListener{
        rita();
        tompos = lablarna.indexOf(tomma);
     }
+    public void FuskScramble(){
+        Collections.clear();
+        Collections.
+    }
     public boolean win(){
         int raknare=0;
         for(JButton l: lablarna){
@@ -196,6 +142,20 @@ public class InlamningsUppgift3 extends JFrame implements ActionListener{
         }
         winst.setText(String.valueOf(true));
         return true;
+    }
+     public void woohoo(){
+        GameBoard.removeAll();
+        JLabel w = new JLabel("Grattis, du vann!");
+        w.setSize(350,350);
+        w.setPreferredSize(new Dimension(355, 355));
+        w.setFont(new Font("Courier New", Font.ITALIC, 25));
+        w.setVisible(true);
+        
+        GameBoard.add(w);
+        GameBoard.setHorizontalAlignment(JLabel.CENTER);
+        invalidate();
+        validate();
+        repaint();
     }
     public void rita(){
         GameBoard.removeAll();
@@ -209,176 +169,29 @@ public class InlamningsUppgift3 extends JFrame implements ActionListener{
         validate();
         repaint();
     }
+   
     
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.println("---------------------");
-//            JButton button = (JButton)e.getSource();
-//         button.setBackground(Color.BLUE);
-//OBS TITTA OVAN DET ÄR SÅ DU REAGERAR MED KNAPPAR DU TRYCKT PÅ
-         if(e.getSource()==ordning){
-             for(JButton l: lablarna)
-                    System.out.print(l.getActionCommand() + ", ");
-                System.out.println("");
-                for(JButton l: lablarna)
-                    System.out.print(l.getActionCommand() + ", ");
-                System.out.println("");
-            
-         }
-         else if(e.getSource()==info){
-              System.out.println("Storleken pa list: " + lablarna.size() + 
-                      ", Senaste klickade: " + teststring01 + ", Gamla klickade: " + tompos);
-         }
-         else if(e.getSource()==scramble){
+        
+         if(e.getSource()==scramble){
               Scramble();
+         }
+         if(e.getSource()==fuskScramble){
+              FuskScramble();
          }
          
          else{
               JButton button = (JButton)e.getSource();
-              //String knappid = e.getActionCommand();
-              int pos = lablarna.indexOf(button);
-              //String namn = button.getName();
-              //int t = tompos;
-              //System.out.println("Du tryckte pa ruta " + pos +", och tompos ligger pa " + tompos);
-              if(pos-tompos == -1 || pos-tompos == 1 || pos-tompos == 4 || pos-tompos == -4 )
+              int pos = lablarna.indexOf(button);if(pos-tompos == -1 || pos-tompos == 1 || pos-tompos == 4 || pos-tompos == -4 ){
                 BytPos(pos);
-              // pos = lablarna.indexOf(button);
-              
-              
-             
-             //detta nedan var här förut
-//             JButton button = (JButton)e.getSource();
-//              //button.setBackground(Color.BLUE);
-//             int aa = lablarna.indexOf(button)+1;
-//             int pekade=Integer.parseInt(e.getActionCommand())-1;
-//             System.out.println("pekade=" + pekade + ", tompos = " + tompos);
-//             if((pekade)==tompos || (pekade+1)==tompos || (pekade+4)==tompos){
-//                 System.out.println("yes");
-//                 System.out.println(e.getActionCommand());
-//             //BytPlats(Integer.valueOf(e.getActionCommand()));}
-//             System.out.println("aa=" +aa);
-//             BytPlats(aa);
-             
+                if(win()){
+                    woohoo();
+                }
+              }
          }
-        
-//        for(JButton l: lablarna){
-//        System.out.print(l.getActionCommand() + ", ");
-//        }
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        
-        
-//        System.out.println(e.toString());
-//        String a=e.getActionCommand();
-//        for(JButton l: lablarna){
-//            if(l.getActionCommand().equals(a)){
-//                l.setBackground(Color.BLUE);
-//                l.setText(Integer.toString(tompos));
-//                
-//            }
-//        }
-//        System.out.println("test01");
-//        System.out.println("Du klickade pa knapp: " + e.getActionCommand());
-//        
-        
-        
-        //BytPlats(Integer.parseInt(e.getActionCommand()));
-        
     }
-//    @Override
-//    public void mouseClicked(MouseEvent e) {
-//        //System.out.println("Nu trycktes "  + e.getSource().toString());
-//        //System.out.println(actionEvent.getActionCommand());
-//        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-//
-//    @Override
-//    public void mousePressed(MouseEvent e) {
-//        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-//
-//    @Override
-//    public void mouseReleased(MouseEvent e) {
-//        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-//
-//    @Override
-//    public void mouseEntered(MouseEvent e) {
-//        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-//
-//    @Override
-//    public void mouseExited(MouseEvent e) {
-//        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-    
+
     public static void main(String[] args){
         InlamningsUppgift3 n = new InlamningsUppgift3();}
-
-    
 }
-
-//class Lyssnaren implements MouseListener{
-//
-//    
-//    
-//}
-
-//public void BytPlats(int a){
-//        teststring01=Integer.toString(a);
-//        //System.out.println(lablarna.toString());
-////                for(JButton l: lablarna)
-////                    System.out.print(l.getActionCommand() + ", ");
-////                System.out.println("");
-//       // System.out.println(lablarna.size() + ", " + a + ", " + tompos);
-//      // System.out.println(tompos);
-//       System.out.println(a-1);
-//       System.out.println(tompos);
-//        Collections.swap(lablarna, (a-1), tompos);
-//        tompos=a-1;
-//        //GameBoard.removeAll();
-//        //SortaAllt(lablarna);
-//        //invalidate();
-//        repaint();
-////        for(JButton l: lablarna)
-////                    System.out.print(l.getActionCommand() + ", ");
-////        System.out.println("");
-//        omgruppera();
-//    }
-//    public void omgruppera(){
-//        GameBoard.removeAll();
-//        for(JButton l: lablarna){
-//            l.removeAll();
-//            //l.removeActionListener(this);
-//            GameBoard.add(l);
-//            //l.addActionListener(this);
-//            l.setVisible(true);
-//            l.setSize(50,50);
-//            l.setPreferredSize(new Dimension(55, 55));
-//        }
-//        //SwingUtilities.updateComponentTreeUI(this);
-//        invalidate();
-//        validate();
-//        repaint();
-//    }
-//    public void SortaAllt(LinkedList listan){
-//        int raknare=0;
-//        //GameBoard.removeAll();
-//        for(JButton l: lablarna){
-//            l.removeAll();
-//            l.removeActionListener(this);
-//            GameBoard.add(l);
-//            //l.addMouseListener(this);
-//            l.addActionListener(this);
-//            l.setVisible(true);
-//            l.setSize(50,50);
-//            l.setPreferredSize(new Dimension(55, 55));
-//            //l.setName(Integer.toString(raknare));
-//            //l.setName("Hejsan");
-//            raknare++;
-//        }
-////        for(JButton l: lablarna){
-////        System.out.print(l.getActionCommand() + ", ");
-////        //l.setText(l.getActionCommand());
-////        }
-//        
-//    }
